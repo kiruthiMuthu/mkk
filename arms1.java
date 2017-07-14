5f128a3d95d48fa0933782439389603fc1020505
kiruthiMuthu <kiruthiIDOK@gmail.com>
2017-07-14 10:26:58 +0530

    Add files via upload

---
+++ arms1.java
@@ -0,0 +1,33 @@
+package arms1;
+
+public class arms1 {
+
+	public static void main(String[] args) {
+		
+		int low = 888, high = 88888;
+
+        for(int number = low + 1; number < high; ++number) {
+            int digits = 0;
+            int result = 0;
+            int originalNumber = number;
+
+            while (originalNumber != 0) {
+                originalNumber /= 10;
+                ++digits;
+            }
+
+            originalNumber = number;
+
+            while (originalNumber != 0) {
+                int remainder = originalNumber % 10;
+                result += Math.pow(remainder, digits);
+                originalNumber /= 10;
+            }
+
+            if (result == number)
+                System.out.print(number + " ");
+        }
+    }
+	}
+
+
